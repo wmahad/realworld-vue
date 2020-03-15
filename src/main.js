9ddf80f44c76253a183459dbd419be6fcc41f300
Mahad <mahad.walusimbi@bettyblocks.com>
2020-03-15 10:55:07 +0100

    Add date picker and set bas structure for the app

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { registerGlobalComps } from './utils/register'
 
-Vue.config.productionTip = false;
+// register global components
+registerGlobalComps(Vue)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,35 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/event/:id',
+    name: 'event-detail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import('../views/EventDetail.vue'),
+    props: true
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: () => import('../views/EventCreate.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
